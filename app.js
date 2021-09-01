29736b56b480b7b911dcfb9cd801cbc9011bf1bb
Kawsar-Hossain-318 <thekawsarhossain@gmail.com>
2021-09-02 03:33:30 +0600

    almost done

--- app.js
+++ app.js
@@ -1,48 +1,60 @@
+// search details id here
+const searchDetails = document.getElementById('search-details');
+// here is the results container
+const booksContainer = document.getElementById('books-container');
+
 // search button's event listerner here 
 document.getElementById('search-button').addEventListener('click', getSearchValue => {
-
     // input text field here
     const searchText = document.getElementById('input-field');
 
+    // error handleing here
+    if(searchText.value === ''){
+        searchDetails.innerText = `Search input can't be empty!`;
+        booksContainer.textContent = '';
+    }else{
     // fetching data here
     const url = (`http://openlibrary.org/search.json?q=${searchText.value}`);
     fetch(url)
     .then(response => response.json())
-    .then(data => displayResult(data.docs))
+    .then(data => displayResults(data))
+    }
 
     // clear the input field here
     searchText.value = '';
 }); 
 
-const displayResult = results => {
+const displayResults = books => {
 
-    // here is the results container
-    const resultsConatiner = document.getElementById('results-container');
+    booksContainer.textContent = '';
 
-    // for Each here
-    results.forEach(details => {
-        console.log(details);
+    books.docs.forEach(book => {
+        // creating new div here
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100">
-        <img src="https://covers.openlibrary.org/b/id/${details.cover_i}-M.jpg" class="card-img-top" alt="...">
+        <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title">Book Name: ${details.title}</h5>
-          <h6 class="card-text">Publisher: ${details.author_name}</h6>
-          <p class="card-text">Published Year: ${details.first_publish_year}</p>
+          <h4 class="card-title">Book Name: ${book.title}</h4>
+          <h6 class="card-text">Publisher: ${book.author_name}</h6>
+          <h6 class="card-text">Published Year: ${book.first_publish_year}</h6>
         </div>
       </div>
         `;
 
         // appending child here
-        resultsConatiner.appendChild(div);
+        booksContainer.appendChild(div);
     });
 
     // get the total elements count from the container
-    const totalELements = resultsConatiner.childElementCount;
+    const totalELements = booksContainer.childElementCount;
     // update the found results count
-    document.getElementById('total-element').innerText = `Total found result's ${totalELements}`;
+    searchDetails.innerText = `Showing ${totalELements} Result's of ${books.numFound}`;
 
-
+    // api error handling here
+    if(books.numFound === 0){
+        console.log('hi');
+        searchDetails.innerText = `No Result's Found !`;
+    }
 }
